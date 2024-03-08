93ccbc538a08a7690c3d9524b41dd7ec4cf39f69
Linen <linenreal@gmail.com>
2024-03-07 23:07:30 -0500

    Update easygo.go

--- easygo.go
+++ easygo.go
@@ -213,34 +213,39 @@ func Encrypt(key []byte, text string) string {
 }
 
 // decrypt from base64 to decrypted string
-func KeyDecrypt(keyStr string, cryptoText string) string {
+func KeyDecrypt(keyStr string, cryptoText string) (string, error) {
 	keyBytes := sha256.Sum256([]byte(keyStr))
 	return Decrypt(keyBytes[:], cryptoText)
 }
 
-func Decrypt(key []byte, cryptoText string) string {
+func Decrypt(key []byte, cryptoText string) (string, error) {
 	ciphertext, err := base64.StdEncoding.DecodeString(cryptoText)
 	if err != nil {
-		panic(err)
+		errmsg := ToString(err)
+		return errmsg, errors.New(errmsg)
 	}
 
 	block, err := aes.NewCipher(key)
 	if err != nil {
-		panic(err)
+		errmsg := ToString(err)
+		return errmsg, errors.New(errmsg)
 	}
 
 	// The IV needs to be unique, but not secure. Therefore it's common to
 	// include it at the beginning of the ciphertext.
 	if len(ciphertext) < aes.BlockSize {
-		panic("ciphertext too short")
+		err := "ciphertext too short"
+		errmsg := ToString(err)
+		return errmsg, errors.New(errmsg)
 	}
+
 	iv := ciphertext[:aes.BlockSize]
 	ciphertext = ciphertext[aes.BlockSize:]
 	stream := cipher.NewCFBDecrypter(block, iv)
 
 	// XORKeyStream can work in-place if the two arguments are the same.
 	stream.XORKeyStream(ciphertext, ciphertext)
-	return fmt.Sprintf("%s", ciphertext)
+	return fmt.Sprintf("%s", ciphertext), nil
 }
 
 // ---------------------------------------------- \\
